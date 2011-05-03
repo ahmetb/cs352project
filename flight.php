c33fedbc1fe6687562064857d256e80b58480f54
Ahmet Alp Balkan <ahmet@ahmetalpbalkan.com>
2011-05-04 01:46:58 +0300

    flights mgmt finished

--- flight.php
+++ flight.php
@@ -31,55 +31,63 @@ if ($action == 'new'){
 		} else {
 			$date_str = "$year-$month-$day $hour:$minute:00";
 
-			
+			mysql_query("BEGIN", $mysql) or die(mysql_error());
 			mysql_query("INSERT INTO flight (flight_number, departure_date, fare, route, plane) VALUES ('$number', '$date_str', $fare, $route, $plane)", $mysql) or die(mysql_error());
-			$flight_id = mysql_result(mysql_query("SELECT last_insert_id() as t", $mysql) or die(mysql_error()), 0, 't');
-			var_dump($flight_id);
+			$flight_id = mysql_insert_id($mysql);
 			if (!$flight_id) {
+				mysql_query("ROLLBACK", $mysql) or die(mysql_error());
 				$error = 'Error occurred during insertion. Try again later.';
 			}
 			else {
-				mysql_query("BEGIN", $mysql) or die(mysql_error());
 				mysql_query("INSERT INTO flight_crew (flight_id, staff_id, role) VALUES ($flight_id, $cpt, 'CPT')", $mysql) or die(mysql_error());
 				mysql_query("INSERT INTO flight_crew (flight_id, staff_id, role) VALUES ($flight_id, $asst, 'ASST')", $mysql) or die(mysql_error());
 				mysql_query("INSERT INTO flight_crew (flight_id, staff_id, role) VALUES ($flight_id, $host, 'HOST')", $mysql) or die(mysql_error());
 				mysql_query("COMMIT", $mysql) or die(mysql_error());
 				$success = 'Flight is arranged!';
 			}
-			
-
 		}
 	}
 } else if ($action == 'edit'){
-		$edit_id = intval($_POST['id']);
-		$name = trim($_POST['name']);
-		$salary = intval(trim($_POST['salary']));
-		$type = trim($_POST['type']);
-		$password = md5(trim($_POST['password']));
-		$day = intval($_POST['day']);
-		$month = intval($_POST['month']);
-		$year = intval($_POST['year']);
+	$edit_id = intval($_POST['id']);
+	$number = trim($_POST['number']);
+	$cpt = intval(trim($_POST['cpt']));
+	$asst = intval(trim($_POST['asst']));
+	$host = intval(trim($_POST['host']));
+	$fare = intval(trim($_POST['fare']));
+	$route = intval(trim($_POST['route']));
+	$plane = intval(trim($_POST['plane']));
+	$day = intval($_POST['day']);
+	$month = intval($_POST['month']);
+	$year = intval($_POST['year']);
+	$hour = intval($_POST['hour']);
+	$minute = intval($_POST['minute']);
 
-		if(!$name || $salary<=0 || !$type || !$day || !$month || !$year){
-			$error = 'Fill in all the fields properly.';
+	if(!$number || $fare<=0 || !$plane || !$route || !$asst || !$cpt || !$host || !$day || !$month || !$year){
+		$error = 'Fill in all the fields.';
+	} else if ($cpt == $asst){
+		$error = 'Captain and asst. pilots cannot be the same.';
+	}else {
+		$count_query = mysql_query("SELECT COUNT(*) FROM flight WHERE flight_number='$number' and id<>$edit_id", $mysql) or die(mysql_error());
+		$count = intval(mysql_result($count_query,0,0));
+
+		if($count > 0){
+			$error = 'A flight with this flight number has already been arranged.';
 		} else {
-			$count_query = mysql_query("SELECT COUNT(*) FROM ground_staff g, flight_staff f where (g.name='$name' AND g.id != $edit_id) OR  (f.name='$name' AND f.id!=$edit_id)", $mysql) or die(mysql_error());
-			$count = intval(mysql_result($count_query,0,0));
+			$date_str = "$year-$month-$day $hour:$minute:00";
 
-			if($count > 0){
-				$error = 'An employee with this name already exists.';
-			} else {
-				$date_str = "$year-$month-$day 00:00:00";
+			mysql_query("BEGIN", $mysql) or die(mysql_error());
+			mysql_query("DELETE FROM flight_crew WHERE flight_id=$edit_id", $mysql) or die(mysql_error());
+			mysql_query("UPDATE flight SET flight_number='$number', departure_date='$date_str', fare=$fare, route=$route, plane=$plane WHERE id=$edit_id", $mysql) or die(mysql_error());
+			mysql_query("INSERT INTO flight_crew (flight_id, staff_id, role) VALUES ($edit_id, $cpt, 'CPT')", $mysql) or die(mysql_error());
+			mysql_query("INSERT INTO flight_crew (flight_id, staff_id, role) VALUES ($edit_id, $asst, 'ASST')", $mysql) or die(mysql_error());
+			mysql_query("INSERT INTO flight_crew (flight_id, staff_id, role) VALUES ($edit_id, $host, 'HOST')", $mysql) or die(mysql_error());
+			mysql_query("COMMIT", $mysql) or die(mysql_error());
+			
+			$success = 'Flight is modified!';
 
-				if ($type=='SALES' || $type=='EXEC'){
-					mysql_query("UPDATE ground_staff SET name='$name',salary=$salary, date_joined='$date_str' WHERE id=$edit_id", $mysql) or die(mysql_error());
-				}else{
-					mysql_query("UPDATE flight_staff SET name='$name',salary=$salary, date_joined='$date_str' WHERE id=$edit_id", $mysql) or die(mysql_error());
-				}
-				$success = 'Employee '.$name.' has been updated.';
-			}
 		}
 	}
+}
 
 if ($action == 'delete'){
 	$delete_id = intval($_GET['id']);
@@ -121,6 +129,14 @@ $sql = "SELECT * FROM plane";
 $results_query = mysql_query($sql, $mysql) or die(mysql_error());
 while ($row = mysql_fetch_assoc($results_query)) $planes[] = $row;
 
+
+
+// Query flights 
+$flights = array();
+$sql = "SELECT f.id, f.flight_number, f.departure_date,f.fare, f.route, f.plane, r.route_name, p.name as plane_name, a.staff_id as asst, c.staff_id as cpt, h.staff_id as host FROM flight f, route_listing r, plane p, flight_crew c, flight_crew a, flight_crew h WHERE r.id=f.route AND p.id=f.plane AND a.role='ASST' and a.flight_id=f.id AND c.role='CPT' and c.flight_id=f.id AND h.role='HOST' and h.flight_id=f.id ORDER BY departure_date DESC";
+
+$results_query = mysql_query($sql, $mysql) or die(mysql_error());
+while ($row = mysql_fetch_assoc($results_query)) $flights[] = $row;
 
 require 'view_flight.php';
 
--- view_flight.php
+++ view_flight.php
@@ -19,48 +19,194 @@ function edit_check(){
 }
 
 
-function edit(id, name, code, city){
+function edit(id, number,fare, y,m,d,h,i, route,plane,cpt,asst,host){
 	$('#edit').slideDown(function(){
 		$('#edit_id').val(id);
-		$('#edit_name').val(name);	
-		$('#edit_code').val(code);	
-		$('#edit_city').val(city);
-		$('#edit_name').focus();		
+		$('#edit_number').val(number);	
+		$('#edit_fare').val(fare);	
+		$('#edit_route').val(route);	
+		$('#edit_plane').val(plane);
+		$('#edit_year').val((y));
+		$('#edit_month').val((m));
+		$('#edit_day').val((d));
+		$('#edit_hour').val(h);
+		$('#edit_minute').val(i);
+		$('#edit_cpt').val(cpt);
+		$('#edit_asst').val(asst);
+		$('#edit_host').val(host);
+				
+		$('#edit_number').focus();		
 	});
 }
 </script>
 
-<h2>Manage Airports</h2>
+<h2>Manage Flights</h2>
 <?php if($success){?><div class='line success'><?=$success?></div><?php }?>
 <?php if($error){?><div class='line error'><?=$error?></div><?php }?>
 
 <table class='data'>
 	<tr class='legend'>
 		<td width='10%'>ID</td>
-		<td width='10%'>Code</td>
-		<td width='30%'>Name</td>
-		<td width='20%'>City</td>
-		<td width='30%'>Operation</td>		
+		<td width='10%'>Flight Number</td>
+		<td width='20%'>Departure</td>
+		<td width='10%'>Fare</td>
+		<td width='20%'>Route</td>		
+		<td width='10%'>Plane</td>		
+		<td width='20%'>Operations</td>		
 	</tr>	
 <?php 
-foreach($results as $result){?>
+foreach($flights as $result){?>
 	<tr>
 		<td width='10%'><?=$result['id']?></td>
-		<td width='10%'><?=$result['code']?></td>
-		<td width='30%'><?=$result['name']?></td>
-		<td width='20%'><?=$result['city']?></td>
-		<td width='50%'>
+		<td width='10%'><?=$result['flight_number']?></td>
+		<td width='20%'><?=date("M d,Y H:i a", strtotime($result['departure_date']))?></td>
+		<td width='10%'>$<?=number_format($result['fare'])?></td>
+		<td width='20%'><?=$result['route_name']?></td>
+		<td width='10%'><?=$result['plane_name']?></td>		
+		<td width='20%'>
 			<a href='javascript://'
 			onclick="edit('<?=$result['id']?>',
-			'<?=$result['name']?>',
-			'<?=$result['code']?>',
-			'<?=$result['city_id']?>'
-			)">[modify]</a>
+			'<?=$result['flight_number']?>',
+			'<?=$result['fare']?>',
+			'<?=date("Y", strtotime($result['departure_date']))?>',
+			'<?=intval(date("m", strtotime($result['departure_date'])))?>',
+			'<?=intval(date("d", strtotime($result['departure_date'])))?>',
+			'<?=intval(date("H", strtotime($result['departure_date'])))?>',		
+			'<?=intval(date("i", strtotime($result['departure_date'])))?>',
+			'<?=$result['route']?>',
+			'<?=$result['plane']?>',
+			'<?=$result['cpt']?>',
+			'<?=$result['asst']?>',
+			'<?=$result['host']?>'
+			);">[modify]</a>
 			<a href='?action=delete&id=<?=$result['id']?>'>[remove]</a>
 		</td>
 	</tr>
 <?php }?>
 </table>
+
+
+
+<div id='edit' style="display: none">
+	<form method='post' action='?action=edit'  onsubmit='return edit_check();'>
+		<input type='hidden' name='id' id='edit_id' value=''/>
+		<fieldset>
+			<legend>Modify Flight</legend>
+			<div class='line'>
+				<div class='label'>Flight Number</div>
+				<div class='field'>
+					<input type='text' name='number' id='edit_number' value=''/>
+				</div>
+			</div>
+			
+			<div class='line'>
+				<div class='label'>Route</div>
+				<div class='field'>
+					<select name='route' id='edit_route'>
+						<?php foreach($routes as $route){?>
+							<option value='<?=$route['id']?>'><?=$route['route_name']?></option>
+						<?php }?>
+					</select>
+				</div>
+			</div>
+			
+			<div class='line'>
+				<div class='label'>Airplane</div>
+				<div class='field'>
+					<select name='plane' id='edit_plane'>
+						<?php foreach($planes as $plane){?>
+							<option value='<?=$plane['id']?>'><?=$plane['name']?> (<?=$plane['model']?> <?=$plane['capacity']?>)</option>
+						<?php }?>
+					</select>
+				</div>
+			</div>
+
+			<div class='line'>
+				<div class='label'>Flight Date </div>
+				<div class='field'>
+					<select name='month' id='edit_month'>
+					<?php for($i=1;$i<=12;$i++){
+						echo "<option ";
+						if(intval(date("m")==$i)) echo "selected='selected' ";
+						echo "value='$i'>".date("M", mktime(0,0,0,$i))."</option>\n";}?>
+					</select> / 
+					<select name='day' id='edit_day'>
+					<?php for($i=1;$i<=31;$i++){
+						echo "<option ";
+						if(intval(date("d"))==$i) echo "selected='selected' ";
+						echo "value='$i'>$i</option>\n";}?>
+					</select> /
+					<select name='year' id='edit_year'>
+					<?php for($i=intval(date("Y"));$i<2020;$i++){
+						echo "<option ";
+						if(intval(date("Y"))==$i) echo "selected='selected' ";
+						echo "value='$i'>$i</option>\n";}?>
+					</select>
+					
+					<select name='hour' id='edit_hour'>
+					<?php for($i=0;$i<24;$i++){
+						echo "<option ";
+						if(intval(date("H"))==$i) echo "selected='selected' ";
+						echo "value='$i'>$i</option>\n";}?>
+					</select>:
+					
+					<select name='minute' id='edit_minute'>
+					<?php for($i=0;$i<60;$i++){
+						echo "<option ";
+						if(intval(date("i"))==$i) echo "selected='selected' ";
+						echo "value='$i'>$i</option>\n";}?>
+					</select>
+				</div>
+			</div>
+			
+			<div class='line'>
+				<div class='label'>Fare ($)</div>
+				<div class='field'>
+					<input type='text' name='fare' id='edit_fare' value=''/>
+				</div>
+			</div>
+		
+			<div class='line'>
+				<div class='label'>Captain Pilot</div>
+				<div class='field'>
+					<select name='cpt' id='edit_cpt'>
+						<?php foreach($pilots as $pilot){?>
+							<option value='<?=$pilot['id']?>'><?=$pilot['name']?></option>
+						<?php }?>
+					</select>
+				</div>
+			</div>
+			
+			<div class='line'>
+				<div class='label'>Asst. Pilot</div>
+				<div class='field'>
+					<select name='asst' id='edit_asst'>
+						<?php foreach($pilots as $pilot){?>
+							<option value='<?=$pilot['id']?>'><?=$pilot['name']?></option>
+						<?php }?>
+					</select>
+				</div>
+			</div>	
+			
+			
+			<div class='line'>
+				<div class='label'>Hostess</div>
+				<div class='field'>
+					<select name='host' id='edit_host'>
+						<?php foreach($hostess as $h){?>
+							<option value='<?=$h['id']?>'><?=$h['name']?></option>
+						<?php }?>
+					</select>
+				</div>
+			</div>
+			
+			<div class='line'>
+				<div class='label'><input type='button' id='cancel' onclick="javascript:$('#edit').slideUp()" value='&laquo; Cancel'/></div>
+				<div class='field'><input type='submit' value='Update &raquo;'/></div>			
+			</div>
+		</fieldset>
+	</form>
+</div>
 
 
 <div id='create'>
@@ -112,7 +258,7 @@ foreach($results as $result){?>
 						echo "value='$i'>$i</option>\n";}?>
 					</select> /
 					<select name='year' id='add_year'>
-					<?php for($i=intval(date("Y"));$i>1990;$i--){
+					<?php for($i=intval(date("Y"));$i<2020;$i++){
 						echo "<option ";
 						if(intval(date("Y"))==$i) echo "selected='selected' ";
 						echo "value='$i'>$i</option>\n";}?>
